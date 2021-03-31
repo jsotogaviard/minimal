bf8bdf35d00fabe29851266097decc54f4503fe6
Jonathan <jonathan.Soto@polestar.eu>
2021-03-31 09:09:07 +0200

    more

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // Imports
 import puppeteer from "puppeteer"
 
-const browser = await puppeteer.launch({ headless: true, devtools: false, slowMo: 0, args: ['--no-sandbox'], executablePath: 'usr/bin/chromium' });
+const browser = await puppeteer.launch({ 
+    headless: true,
+    devtools: false, 
+    slowMo: 0, 
+    args: ['--no-sandbox'], 
+    executablePath: 'usr/bin/chromium-browser' 
+});
 const page = await browser.newPage();
 console.log(page)
